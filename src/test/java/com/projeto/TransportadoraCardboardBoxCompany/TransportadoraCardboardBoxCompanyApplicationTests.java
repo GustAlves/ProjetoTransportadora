07f7411267789c3871071134dee9ec5b05665f2a
Gustavo Alves <gustavoalves.ti@gmail.com>
2020-04-17 21:54:22 -0300

    Projeto criado no STS

---
+++ src/test/java/com/projeto/TransportadoraCardboardBoxCompany/TransportadoraCardboardBoxCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.TransportadoraCardboardBoxCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportadoraCardboardBoxCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
