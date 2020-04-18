2f6312f5f227a7e3a1ff405abebed9505e015cb7
Gustavo Alves <gustavoalves.ti@gmail.com>
2020-04-18 11:25:08 -0300

    Criando banco de dados

--- src/main/java/com/projeto/Transportadora/entities/Empresa.java
+++ src/main/java/com/projeto/Transportadora/entities/Empresa.java
@@ -2,9 +2,17 @@ package com.projeto.Transportadora.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Empresa implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nomeFilial;
 	private Integer tipoTransporte;
