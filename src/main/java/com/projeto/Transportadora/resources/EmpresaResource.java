a111670a8a6f9ac83259e4e540318260c15a0688
Gustavo Alves <gustavoalves.ti@gmail.com>
2020-04-19 10:25:00 -0300

    Criando método de Insert

--- src/main/java/com/projeto/Transportadora/resources/EmpresaResource.java
+++ src/main/java/com/projeto/Transportadora/resources/EmpresaResource.java
@@ -1,13 +1,17 @@
 package com.projeto.Transportadora.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projeto.Transportadora.entities.Empresa;
 import com.projeto.Transportadora.services.EmpresaService;
@@ -31,4 +35,11 @@ public class EmpresaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PostMapping
+	public ResponseEntity<Empresa> insert(@RequestBody Empresa obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
--- src/main/java/com/projeto/Transportadora/services/EmpresaService.java
+++ src/main/java/com/projeto/Transportadora/services/EmpresaService.java
@@ -23,4 +23,8 @@ public class EmpresaService {
 		Optional<Empresa> obj = repository.findById(id);
 		return obj.get();
 	}
+
+	public Empresa insert(Empresa obj) {
+		return repository.save(obj);
+	}
 }
